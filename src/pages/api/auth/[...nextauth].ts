58c9f07c01b4712dd154e104bfed55e655f79ed4
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-09-04 16:45:54 +0800

    Added credentials provider on nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
+import { verifyPassword } from "../../../utils/auth";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -23,7 +25,48 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "john.doe@test.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials, _req) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user) return null;
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password as string
+        );
+
+        if (!isValid) return null;
+
+        if (user) {
+          return {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            image: user.image,
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
 };
 
