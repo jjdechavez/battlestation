aa7b37451012303f8e5365efd7f1e7d8c4488330
John Jerald De Chavez <john.dechavez@stationfive.com>
2022-11-26 16:53:50 +0800

    Add workspace slideover

--- src/pages/dashboard/workspace.tsx
+++ src/pages/dashboard/workspace.tsx
@@ -1,7 +1,8 @@
-import { ReactElement } from 'react';
-import Image from 'next/image';
+import React, { ReactElement } from 'react';
 import Link from 'next/link';
+import { Dialog, Transition } from '@headlessui/react';
 import DashboardLayout from 'components/layout/dashboard';
+import HeroIcon from 'components/base/HeroIcon';
 import { NextPageWithLayout } from 'pages/_app';
 
 const workspaces = [
@@ -23,14 +24,16 @@ const workspaces = [
 ];
 
 const WorkspacePage: NextPageWithLayout = () => {
+  const [showSlideOver, setShowSlideOver] = React.useState(false);
+
   return (
     <section>
       <header className='bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6'>
         <div className='flex items-center justify-between'>
           <h2 className='font-semibold text-slate-900'>Workspace</h2>
-          <a
-            href='/new'
+          <button
             className='hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm'
+            onClick={() => setShowSlideOver(true)}
           >
             <svg
               width='20'
@@ -41,8 +44,8 @@ const WorkspacePage: NextPageWithLayout = () => {
             >
               <path d='M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z' />
             </svg>
-            New
-          </a>
+            New Workspace
+          </button>
         </div>
         <form className='group relative'>
           <svg
@@ -66,6 +69,7 @@ const WorkspacePage: NextPageWithLayout = () => {
           />
         </form>
       </header>
+      <WorkspaceSlideOver show={showSlideOver} handleShow={setShowSlideOver} />
       <ul className='bg-slate-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6'>
         {workspaces.map((workspace) => (
           <li
@@ -119,6 +123,92 @@ const WorkspacePage: NextPageWithLayout = () => {
     </section>
   );
 };
+
+type WorkspaceSlideOverType = {
+  show: boolean;
+  handleShow: (state: boolean) => void;
+};
+
+const WorkspaceSlideOver = ({ show, handleShow }: WorkspaceSlideOverType) => {
+  return (
+    <Transition.Root show={show} as={React.Fragment}>
+      <Dialog as='div' className='relative z-10' onClose={handleShow}>
+        <Transition.Child
+          as={React.Fragment}
+          enter='ease-in-out duration-500'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in-out duration-500'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 overflow-hidden'>
+          <div className='absolute inset-0 overflow-hidden'>
+            <div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10'>
+              <Transition.Child
+                as={React.Fragment}
+                enter='transform transition ease-in-out duration-500 sm:duration-700'
+                enterFrom='translate-x-full'
+                enterTo='translate-x-0'
+                leave='transform transition ease-in-out duration-500 sm:duration-700'
+                leaveFrom='translate-x-0'
+                leaveTo='translate-x-full'
+              >
+                <Dialog.Panel className='pointer-events-auto relative w-screen max-w-md'>
+                  <Transition.Child
+                    as={React.Fragment}
+                    enter='ease-in-out duration-500'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in-out duration-500'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                  >
+                    <div className='absolute top-0 left-0 -ml-8 flex pt-4 pr-2 sm:-ml-10 sm:pr-4'>
+                      <button
+                        type='button'
+                        className='rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white'
+                        onClick={() => handleShow(false)}
+                      >
+                        <span className='sr-only'>Close panel</span>
+                        <HeroIcon
+                          name='XMarkIcon'
+                          className='h-6 w-6'
+                          aria-hidden='true'
+                        />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className='flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl'>
+                    <div className='px-4 sm:px-6'>
+                      <Dialog.Title className='text-lg font-medium text-gray-900'>
+                        New Workspace
+                      </Dialog.Title>
+                    </div>
+                    <div className='relative mt-6 flex-1 px-4 sm:px-6'>
+                      {/* Replace with your content */}
+                      <div className='absolute inset-0 px-4 sm:px-6'>
+                        <div
+                          className='h-full border-2 border-dashed border-gray-200'
+                          aria-hidden='true'
+                        />
+                      </div>
+                      {/* /End replace */}
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
 WorkspacePage.getLayout = function getLayout(page: ReactElement) {
   return <DashboardLayout>{page}</DashboardLayout>;
 };
