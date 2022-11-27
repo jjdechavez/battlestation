aab677965b71fe1bf964ae9855902293b9d66790
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-11-27 11:39:26 +0800

    Implement assign author on workspace router

--- src/server/router/workspace.ts
+++ src/server/router/workspace.ts
@@ -6,7 +6,9 @@ export const workspaceRouter = createProtectedRouter()
     async resolve({ ctx }) {
       const userId = ctx.session.user.id;
 
-      const workspaces = await ctx.prisma.workspace.findMany({});
+      const workspaces = await ctx.prisma.workspace.findMany({
+        where: { authorId: userId },
+      });
       return workspaces;
     },
   })
@@ -16,8 +18,14 @@ export const workspaceRouter = createProtectedRouter()
       type: z.enum(['PERSONAL', 'WORK']),
     }),
     async resolve({ ctx, input }) {
+      const userId = ctx.session.user.id;
+
       const workspace = await ctx.prisma.workspace.create({
-        data: input,
+        data: {
+          title: input.title,
+          type: input.type,
+          author: { connect: { id: userId } },
+        },
       });
 
       return workspace;
