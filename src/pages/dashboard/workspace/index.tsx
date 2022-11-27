350f0e8d29a162577d26bf53f6fd6173f74386fd
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-11-27 12:45:00 +0800

    Moved workspace index and add id worksapce page

--- src/pages/dashboard/workspace/index.tsx
+++ src/pages/dashboard/workspace/index.tsx
@@ -68,10 +68,10 @@ const WorkspacePage: NextPageWithLayout = () => {
         <ul className='bg-slate-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6'>
           {workspaces.data?.map((workspace) => (
             <li
-              key={workspace.title}
+              key={workspace.id}
               className='group cursor-pointer rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md'
             >
-              <Link href='#'>
+              <Link href={`/dashboard/workspace/${workspace.id}`}>
                 <a className='hover:bg-blue-500 hover:shadow-md group rounded-md bg-white shadow-sm'>
                   <dl className='grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center'>
                     <div>
