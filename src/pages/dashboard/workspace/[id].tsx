2d0604cd303de469286cb43908702149d477e081
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-11-27 17:59:50 +0800

    Implement initial slideover on workspace[id] page

--- src/pages/dashboard/workspace/[id].tsx
+++ src/pages/dashboard/workspace/[id].tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react';
 import { useRouter } from 'next/router';
-import DashboardLayout from 'components/layout/dashboard';
-import HeroIcon from 'components/base/HeroIcon';
 import { NextPageWithLayout } from 'pages/_app';
 import { trpc } from 'utils/trpc';
+
+import HeroIcon from 'components/base/HeroIcon';
+import DashboardLayout from 'components/layout/dashboard';
+import SlideOver from 'components/base/SlideOver';
 
 const WorkspacePage: NextPageWithLayout = () => {
   const router = useRouter();
@@ -12,6 +14,7 @@ const WorkspacePage: NextPageWithLayout = () => {
     'workspace.getWorkspace',
     { id: workspaceId },
   ]);
+  const [showSlideOver, setShowSlideOver] = React.useState(false);
 
   if (workspace.isLoading) {
     return <div>Loading...</div>;
@@ -35,9 +38,19 @@ const WorkspacePage: NextPageWithLayout = () => {
           </h2>
         </div>
       </header>
+      <SlideOver
+        name='New Section'
+        show={showSlideOver}
+        handleShow={setShowSlideOver}
+      >
+        <div>Form</div>
+      </SlideOver>
       <ul className='bg-slate-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 gap-4 text-sm leading-6'>
         <li className='flex'>
-          <button className='hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3'>
+          <button
+            className='hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3'
+            onClick={() => setShowSlideOver(true)}
+          >
             <svg
               className='group-hover:text-blue-500 mb-1 text-slate-400'
               width='20'
