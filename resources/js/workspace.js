108f0aea2c84081a8293dc75249caee84a5302a7
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-02-12 11:28:03 +0800

    Seperate onMethod of drag and position api on sortablejs

--- resources/js/workspace.js
+++ resources/js/workspace.js
@@ -10,48 +10,7 @@ htmx.onLoad(function () {
       group: 'sections',
       filter: '.task-option',
       animation: 150,
-      onEnd: (evt) => {
-        if (evt.pullMode) {
-          // Dragged item to other list
-          const toElement = evt.to;
-          const toElementId = toElement.id;
-
-          const fromElement = evt.from;
-          const fromElementId = fromElement.id;
-
-          const fromElementChildren = Array.from(fromElement.children);
-          const fromIds = fromElementChildren.map(
-            (child) => child.querySelector('input').value
-          );
-
-          const toElementChildren = Array.from(toElement.children);
-          const toIds = toElementChildren.map(
-            (child) => child.querySelector('input').value
-          );
-
-          const formElement = toElement.closest('form');
-          const workspaceId = formElement.dataset.workspaceId;
-          const csrfToken = formElement
-            .querySelector("input[name='_csrf']")
-            .attributes.getNamedItem('value').value;
-
-          htmx.ajax('POST', `/dashboard/workspaces/${workspaceId}/drag`, {
-            target: '#section-list',
-            swap: 'outerHTML',
-            values: {
-              sections: JSON.stringify({
-                [toElementId]: toIds,
-                [fromElementId]: fromIds,
-              }),
-            },
-            headers: {
-              'x-csrf-token': csrfToken,
-            },
-          });
-
-          return true;
-        }
-
+      onUpdate: (evt) => {
         const toElement = evt.to;
         const toElementChildren = Array.from(toElement.children);
         const toElementIds = toElementChildren.map(
@@ -79,6 +38,42 @@ htmx.onLoad(function () {
             },
           }
         );
+      },
+      onAdd: (evt) => {
+        const toElement = evt.to;
+        const toElementId = toElement.id;
+
+        const fromElement = evt.from;
+        const fromElementId = fromElement.id;
+
+        const fromElementChildren = Array.from(fromElement.children);
+        const fromIds = fromElementChildren.map(
+          (child) => child.querySelector('input').value
+        );
+
+        const toElementChildren = Array.from(toElement.children);
+        const toIds = toElementChildren.map(
+          (child) => child.querySelector('input').value
+        );
+
+        const formElement = toElement.closest('form');
+        const workspaceId = formElement.dataset.workspaceId;
+        const csrfToken = formElement
+          .querySelector("input[name='_csrf']")
+          .attributes.getNamedItem('value').value;
+
+        htmx.ajax('POST', `/dashboard/workspaces/${workspaceId}/drag`, {
+          target: '#section-list',
+          values: {
+            sections: JSON.stringify({
+              [toElementId]: toIds,
+              [fromElementId]: fromIds,
+            }),
+          },
+          headers: {
+            'x-csrf-token': csrfToken,
+          },
+        });
       },
     });
 
